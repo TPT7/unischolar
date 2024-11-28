a0cd3c3fddc35d63036297465f81a208f95a38d2
TPT <tanatswatavengwa@gmail.com>
2024-11-28 18:03:48 +0200

    testing

--- src/App.js
+++ src/App.js
@@ -49,3 +49,4 @@ const WrappedApp = () => (
 );
 
 export default WrappedApp;
+
--- src/history.js
+++ src/history.js
@@ -9,10 +9,6 @@ const HistoryPage = () => {
           <h2>Questions and Answers History</h2>
           <p>You can access all the previously answered questions here together with the comments that were shared on the question.</p>
         </div>
-        <div id="home" className="section">
-          <div id="questionsList"></div>
-          <div id="commentsList"></div>
-        </div>
       </div>
     </>
   );
